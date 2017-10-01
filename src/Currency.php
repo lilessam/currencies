4eef0e46d0eaf1c141e29e247a3cdc5b386c3e57
Lil Essam <ErwinChafros@gmail.com>
2017-10-01 11:36:13 +0200

    Add google finance API

--- src/Currency.php
+++ src/Currency.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Cache;
 class Currency
 {
     public static $baseUrl = "http://api.fixer.io/latest";
+    public static $api     = config('currencies.api');
 
     /**
      * Convert value from currency to another
@@ -16,16 +17,18 @@ class Currency
      */
     public static function convertCurrency($value, $from, $to)
     {
-        $cache_name = 'rate' . $from . '.' . $to . '.' . $value;
+        $cache_name = 'rate.' . $from . '.' . $to . '.' . $value;
 
         if (Cache::has($cache_name)) {
 
             return Cache::get($cache_name);
 
         } else {
-            $rates  = file_get_contents(self::$baseUrl . "?base=" . $from . "&symbols=" . $to);
-            $rates  = (object) json_decode($rates);
-            $result = $rates->rates->{$to} * $value;
+            if (static::$api == 'fixer') {
+                $result = static::fixerConvertCurrency($value, $from, $to);
+            } elseif (static::$api == 'google') {
+                $result = static::googleConvertCurrency($value, $from, $to);
+            }
 
             Cache::put($cache_name, $result, config('currencies.cache_minutes'));
 
@@ -34,10 +37,57 @@ class Currency
     }
 
     /**
-     * Get rates of value.
+     * Convert currency amount using FixerIO.
+     * @param  float  $value
+     * @param  string $from
+     * @param  string $to
+     * @return float
+     */
+    public static function fixerConvertCurrency(float $value, string $from, string $to) : float
+    {
+        $rates  = file_get_contents(self::$baseUrl . "?base=" . $from . "&symbols=" . $to);
+        $rates  = (object) json_decode($rates);
+        $result = $rates->rates->{$to} * $value;
+
+        return $result;
+    }
+
+    /**
+     * Convert currency amount using Google.
+     * @param  float $amount
+     * @param  string $from_currency
+     * @param  string $to_currency
+     * @return float
+     */
+    public static function googleConvertCurrency(float $amount, string $from_currency, string $to_currency) : float
+    {
+        $amount        = urlencode($amount);
+        $from_currency = urlencode($from_currency);
+        $to_currency   = urlencode($to_currency);
+
+        $url = "https://finance.google.com/finance/converter?a=$amount&from=$from_currency&to=$to_currency";
+
+        $ch      = curl_init();
+        $timeout = 0;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        curl_setopt($ch, CURLOPT_USERAGENT,
+            "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $rawdata = curl_exec($ch);
+        curl_close($ch);
+        $data = explode('bld>', $rawdata);
+        $data = explode($to_currency, $data[1]);
+
+        return round($data[0], 2);
+    }
+
+    /**
+     * Get rates of value. | Fixer IO Only
      * @param  decimal $value
      * @param  string $from
-     * @param  array $to
+     * @param  array|null $to
      * @return json
      */
     public static function getRates($from, $to = null)
--- src/config/currencies.php
+++ src/config/currencies.php
@@ -4,4 +4,6 @@ return [
 	
 	// How many minutes you wanna keep the cache data
     'cache_minutes' => 60,
+    // google or fixer
+    'api'           => 'google'
 ];
